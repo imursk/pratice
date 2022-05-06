920c5ea729d39f926068ccad40cf2267ac8b226c
Heypi <957977526@qq.com>
2022-05-06 22:04:15 +0800

    apply,bind

--- call,apply,bind.js
+++ call,apply,bind.js
@@ -9,7 +9,6 @@ Function.prototype.myCall = function (context) {
     throw new Error("Type error")
   }
   // 首先获取参数
-  console.log(Array.prototype.slice.call(arguments, 1),'arguments');
   let args = [...arguments].slice(1)
   let result = null
   // 判断 context 是否传入，如果没有传就设置为 window
@@ -25,52 +24,54 @@ Function.prototype.myCall = function (context) {
   return result
 }
 
-// Function.prototype.myApply = function (context) {
-//   if (typeof this !== "function") {
-//     throw new Error("Type error")
-//   }
-//   let result = null
-//   context = context || window
-//   // 与上面代码相比，我们使用 Symbol 来保证属性唯一
-//   // 也就是保证不会重写用户自己原来定义在 context 中的同名属性
-//   const fnSymbol = Symbol()
-//   context[fnSymbol] = this
-//   // 执行要被调用的方法
-//   if (arguments[1]) {
-//     result = context[fnSymbol](...arguments[1])
-//   } else {
-//     result = context[fnSymbol]()
-//   }
-//   delete context[fnSymbol]
-//   return result
-// }
+Function.prototype.myApply = function (context) {
+  if (typeof this !== "function") {
+    throw new Error("Type error")
+  }
+  let result = null
+  context = context || window
+  // 与上面代码相比，我们使用 Symbol 来保证属性唯一
+  // 也就是保证不会重写用户自己原来定义在 context 中的同名属性
+  const fnSymbol = Symbol()
+  context[fnSymbol] = this
+  // 执行要被调用的方法
+  if (arguments[1]) {
+    result = context[fnSymbol](...arguments[1])
+  } else {
+    result = context[fnSymbol]()
+  }
+  delete context[fnSymbol]
+  return result
+}
 
 
-// Function.prototype.myBind = function (context) {
-//   // 判断调用对象是否为函数
-//   if (typeof this !== "function") {
-//     throw new Error("Type error")
-//   }
-//   // 获取参数
-//   const args = [...arguments].slice(1),
-//   const fn = this
-//   return function Fn () {
-//     return fn.apply(
-//       this instanceof Fn ? this : context,
-//       // 当前的这个 arguments 是指 Fn 的参数
-//       args.concat(...arguments)
-//     )
-//   }
-// }
+Function.prototype.myBind = function (context) {
+  // 判断调用对象是否为函数
+  if (typeof this !== "function") {
+    throw new Error("Type error")
+  }
+  // 获取参数
+  const args = [...arguments].slice(1)
+  const fn = this
+  return function Fn () {
+    return fn.apply(
+      this instanceof Fn ? this : context,
+      // 当前的这个 arguments 是指 Fn 的参数
+      args.concat(...arguments)
+    )
+  }
+}
 
 
 
 let obj = {
   value: '666',
 }
-function fn () {
-  console.log(this.value)
+function fn (params1, params2) {
+  console.log(this.value, params1, params2)
 }
-fn.myCall(obj)
+// fn.myCall(obj,1,2)
+// fn.myApply(obj, [1, 2])
+fn.myBind(obj, 1, 2)()
 
 
