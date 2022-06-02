369a2f16319fd818968884ce6195907b5b7ca580
zhongkun <zhongkun@51shebao.com>
2022-06-02 15:33:10 +0800

    style(new): 添加注释

--- new.js
+++ new.js
@@ -1,7 +1,11 @@
 function myNew (context) {
+  // 创建一个空对象（需要继承Object上的所有属性和方法）
   const obj = new Object()
+  // 连接原型链
   obj.__proto__ = context.prototype
+  // 修改context的this指向为obj
   const res = context.apply(obj, [...arguments].slice(1))
+  // return context
   return typeof context === 'object' ? res : obj
 }
 
